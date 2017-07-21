ba798ca3541934a4fb8b1e94de5ee7571d854de7
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-07-21 19:40:56 +0900

    validate argument length
    [BREAKING] drop support for Node.js < 6

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
 
-module.exports = function differentPlatform() {
+module.exports = function differentPlatform(...args) {
+  if (args.length !== 0) {
+    throw new RangeError(`Expected no arguments, but got ${args.length} arguments instead.`);
+  }
+
   if (process.platform === 'win32') {
     return 'linux';
   }
--- test.js
+++ test.js
@@ -4,6 +4,16 @@ const differentPlatform = require('.');
 const pretendPlatform = require('pretend-platform');
 const test = require('tape');
 
+test('differentPlatform()', t => {
+  t.throws(
+    () => differentPlatform('a'),
+    /^RangeError.*Expected no arguments, but got 1 arguments instead\./,
+    'should throw an error when it takes any arguments.'
+  );
+
+  t.end();
+});
+
 test('differentPlatform() on Windows', t => {
   pretendPlatform('win32');
 
