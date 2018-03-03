203122962836b818e5862396f328b1bb4b582328
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-03-03 19:42:57 +0900

    update devDependencies
    Highlight: update @shinnn/eslint-config and follow its rules, for exmaple tab indent

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = function differentPlatform(...args) {
-  if (args.length !== 0) {
-    throw new RangeError(`Expected no arguments, but got ${args.length} arguments instead.`);
-  }
+	if (args.length !== 0) {
+		throw new RangeError(`Expected no arguments, but got ${args.length} arguments instead.`);
+	}
 
-  if (process.platform === 'win32') {
-    return 'linux';
-  }
+	if (process.platform === 'win32') {
+		return 'linux';
+	}
 
-  return 'win32';
+	return 'win32';
 };
--- test.js
+++ test.js
@@ -5,25 +5,25 @@ const pretendPlatform = require('pretend-platform');
 const test = require('tape');
 
 test('differentPlatform()', t => {
-  t.throws(
-    () => differentPlatform('a'),
-    /^RangeError.*Expected no arguments, but got 1 arguments instead\./,
-    'should throw an error when it takes any arguments.'
-  );
+	t.throws(
+		() => differentPlatform('a'),
+		/^RangeError.*Expected no arguments, but got 1 arguments instead\./,
+		'should throw an error when it takes any arguments.'
+	);
 
-  t.end();
+	t.end();
 });
 
 test('differentPlatform() on Windows', t => {
-  pretendPlatform('win32');
+	pretendPlatform('win32');
 
-  t.equal(differentPlatform(), 'linux', 'should return `linux`.');
-  t.end();
+	t.equal(differentPlatform(), 'linux', 'should return `linux`.');
+	t.end();
 });
 
 test('differentPlatform() on non-Windows OS', t => {
-  pretendPlatform('linux');
+	pretendPlatform('linux');
 
-  t.equal(differentPlatform(), 'win32', 'should return `win32`.');
-  t.end();
+	t.equal(differentPlatform(), 'win32', 'should return `win32`.');
+	t.end();
 });
